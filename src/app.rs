f5d1d2aaddd2f0be6d7ef5e8ccca929785e48caa
Ajinkya <ajinkyab1987@gmail.com>
2020-06-23 11:54:35 +0530

    display list & added css

--- src/app.rs
+++ src/app.rs
@@ -1,12 +1,15 @@
 use yew::prelude::*;
+extern crate rand;
+// use rand::prelude::*;
 
 pub struct App {
-    counter: i64,
+    items: Vec<i64>,
     link: ComponentLink<Self>,
 }
 
 pub enum Msg {
     AddOne,
+    RemoveOne,
 }
 
 impl Component for App {
@@ -14,15 +17,18 @@ impl Component for App {
     type Properties = ();
 
     //create is the equivalent of a constructor for the component.
-    fn create(props: Self::Properties, link: ComponentLink<Self>) -> Self {
+    fn create(_: Self::Properties, link: ComponentLink<Self>) -> Self {
         App {
             link,
-            counter: 0,
+            items: Vec::new(),
         }
     }
     fn update(&mut self, msg: Self::Message) -> ShouldRender {
         match msg {
-            Msg::AddOne => self.counter += 1,
+            Msg::AddOne => self.items.push(rand::random()),
+            Msg::RemoveOne => {
+                self.items.pop();
+            },
         }
 
         true
@@ -31,10 +37,30 @@ impl Component for App {
         false
     }
     fn view(&self) -> Html {
+        let render_item = |item| {
+            html! {
+                <>
+                    <tr><td>{ item }</td></tr>
+                </>
+            }
+        };
+
         html! {
-            <div>
-                <p>{"Counter: "} {self.counter}</p>
-                <button onclick=self.link.callback(|_| Msg::AddOne)>{"Add One"}</button>
+            <div class="main">
+                <div class="card">
+                    <header>
+                        {"Items test: "}
+                    </header>
+                    <div class="card-body">
+                        <table class="primary">
+                            { for self.items.iter().map(render_item) }
+                        </table>
+                    </div>
+                    <footer>
+                        <button onclick=self.link.callback(|_| Msg::AddOne)>{ "Add 1" }</button> {" "}
+                        <button onclick=self.link.callback(|_| Msg::RemoveOne)>{ "Remove 1" }</button>
+                    </footer>
+                </div>
             </div>
         }
     }
